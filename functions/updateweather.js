054e25263af62ef6ebf9ec9d23303a2c9d38a253
kallanreed <3761006+kallanreed@users.noreply.github.com>
2024-03-16 10:26:40 -0700

    Basic data storage

--- functions/updateweather.js
+++ functions/updateweather.js
@@ -1,4 +1,27 @@
-export function onRequest(context) {
-  console.log("[LOG] " + context.request.url);
-  return new Response("OK");
+export async function onRequest(context) {
+  const parsedUrl = new URL(context.request.url);
+  const params = parsedUrl.searchParams;
+  const mac = params.get('PASSKEY');
+  const data = context.env.TEMPDATA;
+
+  if (context.env.ALLOWED_MAC === undefined) {
+    throw new Error('ALLOWED_MAC undefined');
+  }
+
+  if (mac !== context.env.ALLOWED_MAC) {
+    throw new Error('Unauthorized: ' + mac);
+  }
+
+  const date = params.get('dateutc');
+  const temp = Number(params.get('tempinf'));
+  const humidity = Number(params.get('humidityin'));
+
+  console.log('[LOG] Date:' + date);
+  console.log('[LOG] Temp:' + temp);
+  console.log('[LOG] Humi:' + humidity);
+
+  await data.put(date, JSON.stringify({ t: temp, h: humidity}),
+    { expirationTtl: 2592000 });  // 1 month
+
+  return new Response('OK');
 }
