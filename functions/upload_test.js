e52e2088a84881427aa350d813e70d7d3f14e566
kallanreed <3761006+kallanreed@users.noreply.github.com>
2026-01-13 22:21:47 -0800

    Use octet-stream instead of JSON

--- functions/upload_test.js
+++ functions/upload_test.js
@@ -1,11 +1,30 @@
 export async function onRequest(context) {
   const start = Date.now();
-  const data = await context.request.json();
-  const result = {
-    id: data.id,
-    elapsed: Date.now() - start,
-    read: data.payload.length
-  };
 
-  return Response.json(result);
+  const reader = context.request.body?.getReader();
+  let read = 0;
+
+  if (reader) {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
+      read += value.byteLength;
+    }
+  }
+
+  const elapsed = Date.now() - start;
+
+  return new Response(
+    JSON.stringify({
+      elapsed,
+      read,
+    }),
+    {
+      status: 204, // or 200 if you prefer
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store",
+      },
+    }
+  );
 }
